80b91c03722a56aa011ddc867c4f3aeefbe588f6
umairnazeer <83.mumair@gmail.com>
2020-03-10 11:15:50 -0400

    added allure report config

--- wdio.conf.js
+++ wdio.conf.js
@@ -116,7 +116,7 @@ exports.config = {
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
     framework: 'jasmine',
-    before: function() {
+    before: function () {
         require('@babel/register')
     },
     //
@@ -126,8 +126,15 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
 
+    // Allure reporter
+    reporters: ['spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: true,
+        }]
+    ],
     //
     // Options to be passed to Jasmine.
     jasmineNodeOpts: {
@@ -138,7 +145,7 @@ exports.config = {
         // The Jasmine framework allows interception of each assertion in order to log the state of the application
         // or website depending on the result. For example, it is pretty handy to take a screenshot every time
         // an assertion fails.
-        expectationResultHandler: function(passed, assertion) {
+        expectationResultHandler: function (passed, assertion) {
             // do something
         }
     },
